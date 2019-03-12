d4e90ef95cd35862bdb5b2fdf1f0fb6b7c0bd04c
Kevin Williams <kevin.williams@kwilliams.app>
2019-03-12 06:56:37 -0500

    Added Type endpoints

--- GymManagement.Api/Controllers/TypeController.cs
+++ GymManagement.Api/Controllers/TypeController.cs
@@ -16,12 +16,54 @@ namespace GymManagement.Api.Controllers
 
         public TypeController (GymManagementDataContext context) { _context = context; }
 
+        [HttpGet]
+        public IActionResult GetTypes()
+        {
+            var types = _context.Types.Where(t => t.IsDeleted == false).ToList();
+            return Ok(types);
+        } 
+
         // Get: /api/Type/{category}
         [HttpGet("{category}")]
         public async Task<ActionResult<IEnumerable<Class>>> GetTypesByCategory(string category)
         {
             var types = await _context.Types.Where(type => type.Category == category).ToListAsync();
             return Ok(types);
+        }
+
+        [HttpPost]
+        public IActionResult CreateType([FromBody]DataModel.Type[] types)
+        {
+            foreach(var type in types)
+            {
+                type.Created = DateTime.Now;
+                type.IsDeleted = false;
+
+                _context.Types.Add(type);
+            }
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetTypes), new { }, types);
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult<DataModel.Type>> UpdateTypes([FromBody]DataModel.Type[] types)
+        {
+            try
+            {
+                foreach(var type in types)
+                {
+                    type.Modified = DateTime.Now;
+                    _context.Types.Update(type);
+                }
+                await _context.SaveChangesAsync();
+
+                return Ok(types);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
